64df87c4d9442e540bf3e4418b73a2277ec53a7f
KristaB77 <87663631+KristaB77@users.noreply.github.com>
2021-07-28 06:56:52 +0300

    Add files via upload

---
+++ DayFinder-2/DayFinder/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DayFinder
+//
+//  Created by Arkadijs Makarenko on 26/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var dayTextField: UITextField?
+    @IBOutlet weak var monthTextField: UITextField?
+    @IBOutlet weak var yearTextField: UITextField?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
